b31421eaf644fef0b7614b951a0074de151b7ca4
Ramkumar Illa <nagavenkatasatyaramkumar.illa@nrg.com>
2026-04-18 15:53:00 +0530

    Added new show method

--- main.py
+++ main.py
@@ -51,4 +51,12 @@ async def get_euron_data():
     async for document in cursor:
         iterms.append(euron_helper(document))
     return iterms
+
+@app.get("/euron/showdata")
+async def show_euron_data():
+    iterms = []
+    cursor = euron_data.find({})
+    async for document in cursor:
+        iterms.append(euron_helper(document))
+    return iterms
     
